eb914ebcd6241bb929cb6519064337d79367a805
Jacqueline <jlee4332@gmail.com>
2021-03-18 22:28:15 -0400

    updating

---
+++ test/helpersTest.js
@@ -0,0 +1,25 @@
+
+const { assert } = require('chai');
+
+const { findIdByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findIdByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findIdByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    assert.deepEqual(expectedOutput, user);
+  });
+});
